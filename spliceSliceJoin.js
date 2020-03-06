4e25dc927b02bea1baa2d0ddce931f8ef53c1309
Nazmul999 <mdnazmulhuda1327@gmail.com>
2020-03-07 00:54:09 +0600

    try to understand

---
+++ spliceSliceJoin.js
@@ -0,0 +1,12 @@
+const nums = [1, 55, 3, 4, 5, 6, 77, 8, 9];
+const part = nums.slice(2, 6);
+
+console.log(part);
+
+// const part2 = nums.splice(1, 7);
+
+console.log(part2);
+
+const part3 = nums.join(",");
+
+console.log(part3);
